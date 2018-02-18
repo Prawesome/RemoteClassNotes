6cccb2349dbfe3fd77a58876d44ab9ceced4e3b1
Prejith Premkumar <prajithpremg@gmail.com>
2018-02-18 12:20:38 +0530

    Add download activity

---
+++ app/src/main/java/com/remoteclassnotes/DownloadActivity.java
@@ -0,0 +1,78 @@
+package com.remoteclassnotes;
+
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DownloadActivity extends AppCompatActivity {
+
+    private ListView mFilesList;
+    private List<NoteFile> filesList;
+    private FileAdapter mAdapter;
+
+    private DatabaseReference firebaseDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download);
+
+        firebaseDatabase = FirebaseDatabase.getInstance().getReference().child("files");
+
+        mFilesList = (ListView) findViewById(R.id.list_download_items);
+        filesList = new ArrayList<NoteFile>();
+
+
+        firebaseDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                NoteFile noteFile = dataSnapshot.getValue(NoteFile.class);
+                filesList.add(noteFile);
+                mAdapter = new FileAdapter(DownloadActivity.this, R.layout.item_file, filesList);
+                mFilesList.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+        mFilesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                NoteFile noteFile = mAdapter.getItem(position);
+
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                CustomTabsIntent customTabsIntent = builder.build();
+                customTabsIntent.launchUrl(DownloadActivity.this, Uri.parse(noteFile.getDownloadUrl()));
+                builder.setToolbarColor(R.color.colorPrimaryDark);
+            }
+        });
+    }
+}
