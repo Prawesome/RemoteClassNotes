653a39cbfa19625db446312e1ae488c77c1ef09a
Prejith Premkumar <prajithpremg@gmail.com>
2018-06-04 20:12:27 +0530

    change file structure

--- app/src/main/java/com/remoteclassnotes/NoteFile.java
+++ app/src/main/java/com/remoteclassnotes/NoteFile.java
@@ -5,16 +5,35 @@ package com.remoteclassnotes;
  */
 
 public class NoteFile {
-    public String fileName;
-    public String downloadUrl;
+    private String fileName;
+    private String downloadUrl;
+    private String description;
+    private String subjectName;
 
     public NoteFile() {
 
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
     public NoteFile(String fileName, String downloadUrl) {
         this.fileName = fileName;
         this.downloadUrl = downloadUrl;
+
     }
 
     public String getFileName() {
