9be1f3b28b0614cba38f0de7e66cd70403c72909
Prejith Premkumar <prajithpremg@gmail.com>
2018-06-14 12:06:53 +0300

    implement about activity

---
+++ app/src/main/java/com/remoteclassnotes/AboutActivity.java
@@ -0,0 +1,28 @@
+package com.remoteclassnotes;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AboutActivity extends AppCompatActivity {
+
+    Button mMailButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        mMailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto: prajithpremg@gmail.com"));
+                startActivity(Intent.createChooser(mailIntent, "Contact developer"));
+             }
+        });
+    }
+}
